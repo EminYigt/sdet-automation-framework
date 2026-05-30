1364f95f35792dc0a9e0c72313888ef04eb15c4b
Yigit <eminyigt00@gmail.com>
2026-05-31 00:05:10 +0300

    Run Chrome in headless mode for CI

--- src/test/java/drivers/DriverManager.java
+++ src/test/java/drivers/DriverManager.java
@@ -21,7 +21,11 @@ public class DriverManager {
                 WebDriverManager.chromedriver().setup();
 
                 ChromeOptions options = new ChromeOptions();
-                options.addArguments("--start-maximized");
+
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
 
                 driver.set(new ChromeDriver(options));
             } else {
